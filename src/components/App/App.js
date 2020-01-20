a60b7056d75f60766471ef9577ed4b232f82a6af
Jesstern Rays <jsstrn@users.noreply.github.com>
2020-01-20 16:22:59 +0800

    Move App to components folder

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -18,22 +18,30 @@ function Header() {
 function Social() {
   return (
     <section className="social">
-      <img
-        alt="telegram"
-        src="https://img.icons8.com/windows/64/000000/telegram-app.png"
-      ></img>
-      <img
-        alt="github"
-        src="https://img.icons8.com/windows/64/000000/github.png"
-      ></img>
-      <img
-        alt="twitter"
-        src="https://img.icons8.com/windows/64/000000/twitter.png"
-      ></img>
-      <img
-        alt="email"
-        src="https://img.icons8.com/windows/64/000000/email.png"
-      ></img>
+      <a href="https://t.me/jsstrn">
+        <img
+          alt="telegram"
+          src="https://img.icons8.com/windows/64/000000/telegram-app.png"
+        ></img>
+      </a>
+      <a href="https://github.com/jsstrn">
+        <img
+          alt="github"
+          src="https://img.icons8.com/windows/64/000000/github.png"
+        ></img>
+      </a>
+      <a href="https://twitter.com/jsstrn">
+        <img
+          alt="twitter"
+          src="https://img.icons8.com/windows/64/000000/twitter.png"
+        ></img>
+      </a>
+      <a href="mailto:hello@jsstrn.me">
+        <img
+          alt="email"
+          src="https://img.icons8.com/windows/64/000000/email.png"
+        ></img>
+      </a>
     </section>
   );
 }
@@ -61,7 +69,7 @@ function About() {
   );
 }
 
-function Project({title, description, links: {source, live}}) {
+function Project({ title, description, links: { source, live } }) {
   return (
     <div className="project">
       <header>
