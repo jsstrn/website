bdfea3394b26202bef92333159a92853a1c70eef
Jesstern Rays <jsstrn@users.noreply.github.com>
2020-01-20 17:45:06 +0800

    Update profile photo

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,7 @@ import React from "react";
 function Header() {
   return (
     <header className="main flex-col-center">
-      <img
-        alt="Jesstern Rays"
-        src="https://cdn4.iconfinder.com/data/icons/avatars-21/512/avatar-circle-human-male-5-512.png"
-      />
+      <img alt="Jesstern Rays" src="/profile.jpg" />
       <h1>Jesstern Rays</h1>
       <p>Software Engineer</p>
     </header>
