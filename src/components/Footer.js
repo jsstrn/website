303dd95b42af4bc118294f05ae952dc4990fdf0b
Jesstern Rays <jsstrn@users.noreply.github.com>
2020-01-20 17:29:22 +0800

    Add year to copyright

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 function Footer() {
+  const year = new Date().getFullYear();
   return (
     <footer className="flex-col-center">
       <span role="img" aria-label="Waving Hand">
         🕺
       </span>
       <p>Handcrafted by Jesstern Rays</p>
-      <p>Copyright © Jesstern Rays · All rights reserved</p>
+      <p>Copyright © {year} Jesstern Rays · All rights reserved</p>
     </footer>
   );
 }
