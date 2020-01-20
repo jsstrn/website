f034de5ef8c8b84a49e187740984dc7adc16208a
Jesstern Rays <jsstrn@users.noreply.github.com>
2020-01-20 17:20:03 +0800

    Move projects to data folder

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,46 +1,6 @@
 import React from "react";
 import Project from "./Project";
-
-const projects = [
-  {
-    id: 1,
-    title: "Daily Dad Jokes",
-    description:
-      "Dad jokes delivered to you everyday rain or shine powered by event-driven serverless lambdas.",
-    links: {
-      source: "/#",
-      live: "/#"
-    }
-  },
-  {
-    id: 2,
-    title: "BBC Earth Daily",
-    description:
-      "BBC Earth videos sourced from YouTube delivered daily and powered by event-driven serverless lambdas.",
-    links: {
-      source: "/#",
-      live: "/#"
-    }
-  },
-  {
-    id: 3,
-    title: "dotfiles",
-    description: "A personal configuration for macOS built with Bash scripts.",
-    links: {
-      source: "/#",
-      live: "/#"
-    }
-  },
-  {
-    id: 4,
-    title: "jsstrn.me",
-    description: "The personal website of Jesstern Rays built in React.",
-    links: {
-      source: "/#",
-      live: "/#"
-    }
-  }
-];
+import projects from "../data/projects.json";
 
 function ProjectList() {
   return (
