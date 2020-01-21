828eeff4bd3ab123c98b4950b8a838db4d4d4ac9
Jesstern Rays <jsstrn@users.noreply.github.com>
2020-01-21 15:18:17 +0800

    Convert Footer to use styled-components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,38 @@
 import React, { useState } from "react";
+import styled, { css } from "styled-components";
+
+const containerStyle = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Container = styled.footer`
+  ${containerStyle}
+  margin: 2rem auto 1rem;
+`;
+
+const Image = styled.img`
+  padding: 0.2rem 1.5rem;
+  width: 1.5rem;
+`;
+
+const Text = styled.p`
+  font-size: 0.7rem;
+  line-height: 0.1em;
+  color: grey;
+`;
 
 function Footer() {
   const year = new Date().getFullYear();
   const [image, setImageSource] = useState("/images/dancer.png");
 
   return (
-    <footer className="flex-col-center">
-      <img src={image} onClick={() => setImageSource("/images/dancer.gif")} />
-      <p>Handcrafted by Jesstern Rays</p>
-      <p>Copyright © {year} Jesstern Rays · All rights reserved</p>
-    </footer>
+    <Container>
+      <Image src={image} onClick={() => setImageSource("/images/dancer.gif")} />
+      <Text>Handcrafted by Jesstern Rays</Text>
+      <Text>Copyright © {year} Jesstern Rays · All rights reserved</Text>
+    </Container>
   );
 }
 
