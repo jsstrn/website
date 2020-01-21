57f937f966bfc36327a915add826d791e49a69f2
Jesstern Rays <jsstrn@users.noreply.github.com>
2020-01-21 14:13:09 +0800

    Load images locally

--- src/components/Social.js
+++ src/components/Social.js
@@ -4,28 +4,16 @@ function Social() {
   return (
     <section className="social">
       <a href="https://t.me/jsstrn">
-        <img
-          alt="telegram"
-          src="https://img.icons8.com/windows/64/000000/telegram-app.png"
-        ></img>
+        <img alt="telegram" src="images/telegram-app.png"></img>
       </a>
       <a href="https://github.com/jsstrn">
-        <img
-          alt="github"
-          src="https://img.icons8.com/windows/64/000000/github.png"
-        ></img>
+        <img alt="github" src="images/github.png"></img>
       </a>
       <a href="https://twitter.com/jsstrn">
-        <img
-          alt="twitter"
-          src="https://img.icons8.com/windows/64/000000/twitter.png"
-        ></img>
+        <img alt="twitter" src="images/twitter.png"></img>
       </a>
       <a href="mailto:hello@jsstrn.me">
-        <img
-          alt="email"
-          src="https://img.icons8.com/windows/64/000000/email.png"
-        ></img>
+        <img alt="email" src="images/email.png"></img>
       </a>
     </section>
   );
