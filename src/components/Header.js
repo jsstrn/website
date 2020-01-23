8988523a70536d14bffab2641277946f8a55d14a
Jesstern Rays <jsstrn@users.noreply.github.com>
2020-01-23 15:05:05 +0800

    Move Header styles to styled components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,28 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.header`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Image = styled.img`
+  width: 10rem;
+  border-radius: 50%;
+`;
+
+const Heading = styled.h1`
+  margin-bottom: 0;
+`;
 
 function Header() {
   return (
-    <header className="main flex-col-center">
-      <img alt="Jesstern Rays" src="/images/profile-yellow.jpg" />
-      <h1>Jesstern Rays</h1>
+    <Container>
+      <Image alt="Jesstern Rays" src="/images/profile-yellow.jpg" />
+      <Heading>Jesstern Rays</Heading>
       <p>Software Engineer</p>
-    </header>
+    </Container>
   );
 }
 
