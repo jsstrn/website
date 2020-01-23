ad1770920bb3fadfef3ffeae715a0f4b9768a714
Jesstern Rays <jsstrn@users.noreply.github.com>
2020-01-23 17:41:22 +0800

    Update about segment

--- src/components/About.js
+++ src/components/About.js
@@ -12,11 +12,21 @@ function About() {
       <div className="about">
         <p>
           I believe in delivering quality software through test driven
-          development and clean code.
+          development and clean code. I've developed both front and backend
+          applications for over a decade and worked with clients from small
+          startups to large enterprises.
         </p>
         <p>
-          I love nature walks, yoga by the beach, and beautiful sunsets. I also
-          like to play board games. I might have a mild addiction to emojis.
+          I am a co-organizer of{" "}
+          <a href="https://www.meetup.com/Junior-Developers-Singapore">
+            Junior Developers Singapore
+          </a>{" "}
+          and remain deeply involved in the tech community in Singapore. I also
+          enjoy contributing to open source projects.
+        </p>
+        <p>
+          When not programming, I like board games, yoga by the beach, and
+          emojis.
         </p>
       </div>
     </section>
