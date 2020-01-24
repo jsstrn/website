881f87900b6342a7e58784ae8e877fb1f0da0b97
Jesstern Rays <jsstrn@users.noreply.github.com>
2020-01-24 15:12:40 +0800

    Add programmer quote when dancer is clicked

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,25 +1,39 @@
 import React, { useState } from "react";
-import { Text } from "./Text";
+import { Text, Quote, Attribution } from "./Text";
 import { Image } from "./Image";
 import { Container } from "./Container";
 
 function Footer() {
   const year = new Date().getFullYear();
   const [image, setImageSource] = useState("/images/dancer.png");
+  const [quote, setQuote] = useState("");
+
+  const fetchQuote = () => {
+    fetch("https://programming-quotes-api.herokuapp.com/quotes/random")
+      .then(res => res.json())
+      .then(data => {
+        setQuote(`"${data.en}" by ${data.author}`);
+        window.scrollTo(0, document.body.scrollHeight);
+      });
+  };
 
   return (
     <Container>
+      {!!quote && <Quote>{quote}</Quote>}
       <Image
         alt="dancer"
         src={image}
-        onClick={() => setImageSource("/images/dancer.gif")}
+        onClick={() => {
+          fetchQuote();
+          setImageSource("/images/dancer.gif");
+        }}
       />
       <Text>Handcrafted by Jesstern Rays</Text>
-      <Text>
-        Copyright © {year} Jesstern Rays · All rights reserved · Icons by{" "}
-        <a href="https://icons8.com">Icons8</a> · Emoji by{" "}
+      <Text>Copyright © {year} Jesstern Rays · All rights reserved ·</Text>
+      <Attribution>
+        Icons by <a href="https://icons8.com">Icons8</a> · Emoji by{" "}
         <a href="https://www.joypixels.com/">JoyPixels</a>
-      </Text>
+      </Attribution>
     </Container>
   );
 }
--- src/components/Footer/Text.js
+++ src/components/Footer/Text.js
@@ -1,7 +1,17 @@
 import styled from "styled-components";
 
 export const Text = styled.p`
+  margin: 0.1rem;
+  text-align: center;
   font-size: 0.7rem;
-  line-height: 0.1em;
   color: grey;
+`;
+
+export const Attribution = styled(Text)`
+  font-size: 0.5rem;
+`;
+
+export const Quote = styled(Text)`
+  padding: 1rem;
+  background-color: whitesmoke;
 `;
