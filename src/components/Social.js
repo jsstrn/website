8afb63ffb979f815aad269b147071b57d7434e42
Jesstern Rays <jsstrn@users.noreply.github.com>
2020-02-13 11:10:49 +0800

    Use styled components for Social component

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,15 +1,38 @@
 import React from "react";
 import { social } from "../data";
+import styled from "styled-components";
+
+const Section = styled.section`
+  margin: 1rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
+
+const Icon = styled.img`
+  padding: 0.5rem;
+  margin: 1rem;
+  width: 22px;
+  height: 22px;
+  border-radius: 50%;
+  background-color: whitesmoke;
+  transition: background-color 0.5s;
+
+  &:hover {
+    cursor: pointer;
+    background-color: lightgrey;
+  }
+`;
 
 function Social() {
   return (
-    <section className="social">
+    <Section>
       {social.map(s => (
         <a key={s.id} href={s.link}>
-          <img alt={s.name} src={`images/${s.icon}`}></img>
+          <Icon alt={s.name} src={`images/${s.icon}`} />
         </a>
       ))}
-    </section>
+    </Section>
   );
 }
 
