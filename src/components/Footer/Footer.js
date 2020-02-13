2c72e4817de38ea2aae8d5829a22f0d144650579
Jesstern Rays <jsstrn@users.noreply.github.com>
2020-02-13 14:19:17 +0800

    Remove dependency on external quotes API

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,31 +1,29 @@
 import React, { useState } from "react";
 import { Text, Quote, Attribution, Image, Container } from "./styledComponents";
+import { quotes } from "../../data";
 
 function Footer() {
   const year = new Date().getFullYear();
   const [image, setImageSource] = useState("/images/dancer.png");
   const [quote, setQuote] = useState("");
 
-  const fetchQuote = () => {
-    fetch("https://programming-quotes-api.herokuapp.com/quotes/random")
-      .then(res => res.json())
-      .then(data => {
-        setQuote(`"${data.en}" by ${data.author}`);
-        window.scrollTo(0, document.body.scrollHeight);
-      });
+  const scrollToBottomOfPage = () => {
+    const element = document.scrollingElement || document.body;
+    element.scrollTop = element.scrollHeight;
+  };
+
+  const activateEasterEgg = () => {
+    setTimeout(() => {
+      setQuote(`${quotes[0].quote} ― ${quotes[0].author}`);
+      setImageSource("/images/dancer.gif");
+      scrollToBottomOfPage();
+    }, 0);
   };
 
   return (
     <Container>
       {!!quote && <Quote>{quote}</Quote>}
-      <Image
-        alt="dancer"
-        src={image}
-        onClick={() => {
-          fetchQuote();
-          setImageSource("/images/dancer.gif");
-        }}
-      />
+      <Image alt="dancer" src={image} onClick={activateEasterEgg} />
       <Text>Handcrafted by Jesstern Rays</Text>
       <Text>Copyright © {year} Jesstern Rays · All rights reserved ·</Text>
       <Attribution>
