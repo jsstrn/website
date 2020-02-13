9aee494b18d60bbcb07abad9108d71278bf75c5d
Jesstern Rays <jsstrn@users.noreply.github.com>
2020-02-13 15:21:49 +0800

    Build project in docs folder

---
+++ docs/precache-manifest.812331f20c9ef2f4f5554a0250ac35ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35fff3d3456eee7a6b6c0e42b2335350",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0753a4d50d45fefe239",
+    "url": "/static/css/main.34e97d4d.chunk.css"
+  },
+  {
+    "revision": "3ad105e5f6ec2ef789b0",
+    "url": "/static/js/2.fce23168.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.fce23168.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a0753a4d50d45fefe239",
+    "url": "/static/js/main.c86e6f3e.chunk.js"
+  },
+  {
+    "revision": "b1bb0f709c1bf53bea2d",
+    "url": "/static/js/runtime-main.732f09b6.js"
+  }
+]);
